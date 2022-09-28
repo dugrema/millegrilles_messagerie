7ed624df8968109c4cb2e917215252f2be678d2f
Mathieu Dugre <mathieu.dugre@mdugre.info>
2022-09-28 14:31:09 -0400

    refact messagerie
    Fix warnings

--- src/domaines_messagerie.rs
+++ src/domaines_messagerie.rs
@@ -1,21 +1,20 @@
 //! Module SenseursPassifs de millegrilles installe sur un noeud 3.protege.
 
 use std::collections::HashMap;
-use std::sync::{Arc, Mutex};
+use std::sync::Arc;
 
 use log::{debug, error, info, warn};
 use millegrilles_common_rust::certificats::ValidateurX509;
 use millegrilles_common_rust::chrono as chrono;
-use millegrilles_common_rust::configuration::{charger_configuration, ConfigMessages, IsConfigNoeud};
+use millegrilles_common_rust::configuration::{charger_configuration, IsConfigNoeud};
 use millegrilles_common_rust::domaines::GestionnaireDomaine;
 use millegrilles_common_rust::futures::stream::FuturesUnordered;
 use millegrilles_common_rust::generateur_messages::GenerateurMessages;
 use millegrilles_common_rust::middleware::Middleware;
 use millegrilles_common_rust::middleware_db::{MiddlewareDb, preparer_middleware_db};
 use millegrilles_common_rust::mongo_dao::MongoDao;
-use millegrilles_common_rust::rabbitmq_dao::{Callback, EventMq, QueueType};
 use millegrilles_common_rust::recepteur_messages::TypeMessage;
-use millegrilles_common_rust::tokio::{sync::{mpsc, mpsc::{Receiver, Sender}}, time::{Duration as DurationTokio, timeout}};
+use millegrilles_common_rust::tokio::{sync::mpsc::{Receiver, Sender}, time::Duration as DurationTokio};
 use millegrilles_common_rust::tokio::spawn;
 use millegrilles_common_rust::tokio::task::JoinHandle;
 use millegrilles_common_rust::tokio::time::sleep;
@@ -23,7 +22,6 @@ use millegrilles_common_rust::tokio_stream::StreamExt;
 use millegrilles_common_rust::transactions::resoumettre_transactions;
 
 use crate::gestionnaire::GestionnaireMessagerie;
-use crate::pompe_messages::PompeMessages;
 
 const DUREE_ATTENTE: u64 = 20000;
 
@@ -54,20 +52,16 @@ pub async fn run() {
 /// Conserve les gestionnaires dans la variable GESTIONNAIRES 'static
 fn charger_gestionnaire() -> &'static TypeGestionnaire {
     // Charger une version simplifiee de la configuration - on veut le certificat associe a l'enveloppe privee
-    let config = charger_configuration().expect("config");
-    let config_noeud = config.get_configuration_noeud();
-    let elastic_search_url = match &config_noeud.elastic_search_url {
-        Some(inner) => inner,
-        None => panic!("URL MG_ELASTICSEARCH_URL doit etre fourni")
-    };
+    // let config = charger_configuration().expect("config");
+    // let config_noeud = config.get_configuration_noeud();
+    // let elastic_search_url = match &config_noeud.elastic_search_url {
+    //     Some(inner) => inner,
+    //     None => panic!("URL MG_ELASTICSEARCH_URL doit etre fourni")
+    // };
 
     // Inserer les gestionnaires dans la variable static - permet d'obtenir lifetime 'static
     unsafe {
         GESTIONNAIRE = TypeGestionnaire::PartitionConsignation(Arc::new(GestionnaireMessagerie::new() ));
-
-        // let mut vec_gestionnaires = Vec::new();
-        // vec_gestionnaires.extend(&GESTIONNAIRES);
-        // vec_gestionnaires
         &GESTIONNAIRE
     }
 }
@@ -75,45 +69,18 @@ fn charger_gestionnaire() -> &'static TypeGestionnaire {
 async fn build(gestionnaire: &'static TypeGestionnaire) -> (FuturesUnordered<JoinHandle<()>>, Arc<MiddlewareDb>) {
 
     // Recuperer configuration des Q de tous les domaines
-    let queues = {
-        let mut queues: Vec<QueueType> = Vec::new();
-        //for g in gestionnaires.clone() {
-            match gestionnaire {
-                TypeGestionnaire::PartitionConsignation(g) => {
-                    queues.extend(g.preparer_queues());
-                },
-                TypeGestionnaire::None => ()
-            }
-        //}
-        queues
-    };
-
-    // Listeners de connexion MQ
-    let (tx_entretien, rx_entretien) = mpsc::channel(1);
-    let listeners = {
-        let mut callbacks: Callback<EventMq> = Callback::new();
-        callbacks.register(Box::new(move |event| {
-            debug!("Callback sur connexion a MQ, event : {:?}", event);
-            let tx_ref = tx_entretien.clone();
-            let _ = spawn(async move {
-                match tx_ref.send(event).await {
-                    Ok(_) => (),
-                    Err(e) => error!("Erreur queuing via callback : {:?}", e)
-                }
-            });
-        }));
-
-        Some(Mutex::new(callbacks))
-    };
-
-    // Preparer middleware avec acces direct aux tables Pki (le domaine est local)
-    // let (
-    //     middleware,
-    //     rx_messages_verifies,
-    //     rx_messages_verif_reply,
-    //     rx_triggers,
-    //     future_recevoir_messages
-    // ) = preparer_middleware_db(queues, listeners);
+    // let queues = {
+    //     let mut queues: Vec<QueueType> = Vec::new();
+    //     //for g in gestionnaires.clone() {
+    //         match gestionnaire {
+    //             TypeGestionnaire::PartitionConsignation(g) => {
+    //                 queues.extend(g.preparer_queues());
+    //             },
+    //             TypeGestionnaire::None => ()
+    //         }
+    //     //}
+    //     queues
+    // };
 
     let middleware_hooks = preparer_middleware_db();
     let middleware = middleware_hooks.middleware;
@@ -121,8 +88,6 @@ async fn build(gestionnaire: &'static TypeGestionnaire) -> (FuturesUnordered<Joi
     // Preparer les green threads de tous les domaines/processus
     let mut futures = FuturesUnordered::new();
     {
-        let mut map_senders: HashMap<String, Sender<TypeMessage>> = HashMap::new();
-
         // ** Domaines **
         {
             let futures_g = match gestionnaire {
@@ -134,21 +99,8 @@ async fn build(gestionnaire: &'static TypeGestionnaire) -> (FuturesUnordered<Joi
             futures.extend(futures_g);        // Deplacer vers futures globaux
         }
 
-        // ** Wiring global **
-
-        // // Creer consommateurs MQ globaux pour rediriger messages recus vers Q internes appropriees
-        // futures.push(spawn(
-        //     consommer(middleware.clone(), middleware_hooks.rx_messages_verifies, map_senders.clone())
-        // ));
-        // futures.push(spawn(
-        //     consommer(middleware.clone(), middleware_hooks.rx_messages_verif_reply, map_senders.clone())
-        // ));
-        // futures.push(spawn(
-        //     consommer(middleware.clone(), middleware_hooks.rx_triggers, map_senders.clone())
-        // ));
-
         // ** Thread d'entretien **
-        futures.push(spawn(entretien(middleware.clone(), rx_entretien, vec![gestionnaire])));
+        futures.push(spawn(entretien(middleware.clone(), vec![gestionnaire])));
 
         // Thread ecoute et validation des messages
         for f in middleware_hooks.futures {
@@ -166,7 +118,7 @@ async fn executer(mut futures: FuturesUnordered<JoinHandle<()>>) {
 }
 
 /// Thread d'entretien
-async fn entretien<M>(middleware: Arc<M>, mut rx: Receiver<EventMq>, gestionnaires: Vec<&'static TypeGestionnaire>)
+async fn entretien<M>(middleware: Arc<M>, gestionnaires: Vec<&'static TypeGestionnaire>)
     where M: Middleware
 {
     let mut certificat_emis = false;
@@ -187,7 +139,7 @@ async fn entretien<M>(middleware: Arc<M>, mut rx: Receiver<EventMq>, gestionnair
         coll_docs_strings
     };
 
-    let mut rechiffrage_complete = false;
+    // let mut rechiffrage_complete = false;
 
     let mut prochain_chargement_certificats_maitredescles = chrono::Utc::now();
     let intervalle_chargement_certificats_maitredescles = chrono::Duration::minutes(5);
@@ -195,17 +147,17 @@ async fn entretien<M>(middleware: Arc<M>, mut rx: Receiver<EventMq>, gestionnair
     let mut prochain_entretien_transactions = chrono::Utc::now();
     let intervalle_entretien_transactions = chrono::Duration::minutes(5);
 
-    let mut prochain_sync = chrono::Utc::now();
-    let intervalle_sync = chrono::Duration::hours(6);
+    // let mut prochain_sync = chrono::Utc::now();
+    // let intervalle_sync = chrono::Duration::hours(6);
 
-    let mut prochaine_confirmation_ca = chrono::Utc::now();
-    let intervalle_confirmation_ca = chrono::Duration::minutes(15);
+    // let mut prochaine_confirmation_ca = chrono::Utc::now();
+    // let intervalle_confirmation_ca = chrono::Duration::minutes(15);
 
-    let mut prochain_chargement_certificats_autres = chrono::Utc::now();
-    let intervalle_chargement_certificats_autres = chrono::Duration::minutes(5);
+    // let mut prochain_chargement_certificats_autres = chrono::Utc::now();
+    // let intervalle_chargement_certificats_autres = chrono::Duration::minutes(5);
 
-    let mut prochain_entretien_elasticsearch = chrono::Utc::now();
-    let intervalle_entretien_elasticsearch = chrono::Duration::minutes(5);
+    // let mut prochain_entretien_elasticsearch = chrono::Utc::now();
+    // let intervalle_entretien_elasticsearch = chrono::Duration::minutes(5);
 
     info!("domaines_messagerie.entretien : Debut thread dans 5 secondes");
 
@@ -217,53 +169,18 @@ async fn entretien<M>(middleware: Arc<M>, mut rx: Receiver<EventMq>, gestionnair
         debug!("domaines_messagerie.entretien  Execution task d'entretien Core {:?}", maintenant);
 
         if prochain_chargement_certificats_maitredescles < maintenant {
-            let enveloppe_privee = middleware.get_enveloppe_privee().clone();
-            let enveloppe_certificat = enveloppe_privee.enveloppe.clone();
             match middleware.charger_certificats_chiffrage(middleware.as_ref()).await {
                 Ok(()) => {
                     prochain_chargement_certificats_maitredescles = maintenant + intervalle_chargement_certificats_maitredescles;
                 },
                 Err(e) => info!("Erreur chargement certificats de maitre des cles tiers : {:?}", e)
             }
-            // match middleware.charger_certificats_chiffrage(middleware.get_enveloppe_privee().enveloppe.as_ref()).await {
-            //     Ok(()) => {
-            //         prochain_chargement_certificats_maitredescles = maintenant + intervalle_chargement_certificats_maitredescles;
-            //         debug!("Prochain chargement cert maitredescles: {:?}", prochain_chargement_certificats_maitredescles);
-            //     },
-            //     Err(e) => warn!("Erreur chargement certificats de maitre des cles : {:?}", e)
-            // }
         }
 
         // Sleep jusqu'au prochain entretien ou evenement MQ (e.g. connexion)
         debug!("domaines_messagerie.entretien Fin cycle, sleep {} secondes", DUREE_ATTENTE / 1000);
         let duration = DurationTokio::from_millis(DUREE_ATTENTE);
-
-        let result = timeout(duration, rx.recv()).await;
-        match result {
-            Ok(inner) => {
-                debug!("domaines_messagerie.entretien Recu event MQ : {:?}", inner);
-                match inner {
-                    Some(e) => {
-                        match e {
-                            EventMq::Connecte => {
-                            },
-                            EventMq::Deconnecte => {
-                                // Reset flag certificat
-                                certificat_emis = false;
-                            }
-                        }
-                    },
-                    None => {
-                        warn!("domaines_messagerie.entretien MQ n'est pas disponible, on ferme");
-                        break
-                    },
-                }
-
-            },
-            Err(_) => {
-                debug!("domaines_messagerie.entretien entretien Timeout, entretien est du");
-            }
-        }
+        sleep(duration).await;
 
         middleware.entretien_validateur().await;
 
@@ -294,7 +211,7 @@ async fn entretien<M>(middleware: Arc<M>, mut rx: Receiver<EventMq>, gestionnair
 
         for g in &gestionnaires {
             match g {
-                TypeGestionnaire::PartitionConsignation(g) => {
+                TypeGestionnaire::PartitionConsignation(_g) => {
                     debug!("Entretien Messagerie noeud protege");
                     // if prochain_entretien_elasticsearch < maintenant {
                     //     prochain_entretien_elasticsearch = maintenant + intervalle_entretien_elasticsearch;
@@ -316,61 +233,61 @@ async fn entretien<M>(middleware: Arc<M>, mut rx: Receiver<EventMq>, gestionnair
     }
 
     // panic!("Forcer fermeture");
-    info!("domaines_messagerie.entretien : Fin thread");
+    // info!("domaines_messagerie.entretien : Fin thread");
 }
 
-async fn consommer(
-    _middleware: Arc<impl ValidateurX509 + GenerateurMessages + MongoDao>,
-    mut rx: Receiver<TypeMessage>,
-    map_senders: HashMap<String, Sender<TypeMessage>>
-) {
-    info!("domaines_messagerie.consommer : Debut thread, mapping : {:?}", map_senders.keys());
-
-    while let Some(message) = rx.recv().await {
-        match &message {
-            TypeMessage::Valide(m) => {
-                warn!("domaines_messagerie.consommer: Message valide sans routing key/action : {:?}", m.message);
-            },
-            TypeMessage::ValideAction(m) => {
-                let contenu = &m.message;
-                let rk = m.routing_key.as_str();
-                let action = m.action.as_str();
-                let domaine = m.domaine.as_str();
-                let nom_q = m.q.as_str();
-                info!("domaines_messagerie.consommer: Traiter message valide (action: {}, rk: {}, q: {})", action, rk, nom_q);
-                debug!("domaines_messagerie.consommer: Traiter message valide contenu {:?}", contenu);
-
-                // Tenter de mapper avec le nom de la Q (ne fonctionnera pas pour la Q de reponse)
-                let sender = match map_senders.get(nom_q) {
-                    Some(sender) => {
-                        debug!("domaines_messagerie.consommer Mapping message avec nom_q: {}", nom_q);
-                        sender
-                    },
-                    None => {
-                        match map_senders.get(domaine) {
-                            Some(sender) => {
-                                debug!("domaines_messagerie.consommer Mapping message avec domaine: {}", domaine);
-                                sender
-                            },
-                            None => {
-                                error!("domaines_messagerie.consommer Message de queue ({}) et domaine ({}) inconnu, on le drop", nom_q, domaine);
-                                continue  // On skip
-                            },
-                        }
-                    }
-                };
-
-                match sender.send(message).await {
-                    Ok(()) => (),
-                    Err(e) => {
-                        error!("domaines_messagerie.consommer Erreur consommer message {:?}", e)
-                    }
-                }
-            },
-            TypeMessage::Certificat(_) => (),  // Rien a faire
-            TypeMessage::Regeneration => (),   // Rien a faire
-        }
-    }
-
-    info!("domaines_messagerie.consommer: Fin thread : {:?}", map_senders.keys());
-}
+// async fn consommer(
+//     _middleware: Arc<impl ValidateurX509 + GenerateurMessages + MongoDao>,
+//     mut rx: Receiver<TypeMessage>,
+//     map_senders: HashMap<String, Sender<TypeMessage>>
+// ) {
+//     info!("domaines_messagerie.consommer : Debut thread, mapping : {:?}", map_senders.keys());
+//
+//     while let Some(message) = rx.recv().await {
+//         match &message {
+//             TypeMessage::Valide(m) => {
+//                 warn!("domaines_messagerie.consommer: Message valide sans routing key/action : {:?}", m.message);
+//             },
+//             TypeMessage::ValideAction(m) => {
+//                 let contenu = &m.message;
+//                 let rk = m.routing_key.as_str();
+//                 let action = m.action.as_str();
+//                 let domaine = m.domaine.as_str();
+//                 let nom_q = m.q.as_str();
+//                 info!("domaines_messagerie.consommer: Traiter message valide (action: {}, rk: {}, q: {})", action, rk, nom_q);
+//                 debug!("domaines_messagerie.consommer: Traiter message valide contenu {:?}", contenu);
+//
+//                 // Tenter de mapper avec le nom de la Q (ne fonctionnera pas pour la Q de reponse)
+//                 let sender = match map_senders.get(nom_q) {
+//                     Some(sender) => {
+//                         debug!("domaines_messagerie.consommer Mapping message avec nom_q: {}", nom_q);
+//                         sender
+//                     },
+//                     None => {
+//                         match map_senders.get(domaine) {
+//                             Some(sender) => {
+//                                 debug!("domaines_messagerie.consommer Mapping message avec domaine: {}", domaine);
+//                                 sender
+//                             },
+//                             None => {
+//                                 error!("domaines_messagerie.consommer Message de queue ({}) et domaine ({}) inconnu, on le drop", nom_q, domaine);
+//                                 continue  // On skip
+//                             },
+//                         }
+//                     }
+//                 };
+//
+//                 match sender.send(message).await {
+//                     Ok(()) => (),
+//                     Err(e) => {
+//                         error!("domaines_messagerie.consommer Erreur consommer message {:?}", e)
+//                     }
+//                 }
+//             },
+//             TypeMessage::Certificat(_) => (),  // Rien a faire
+//             TypeMessage::Regeneration => (),   // Rien a faire
+//         }
+//     }
+//
+//     info!("domaines_messagerie.consommer: Fin thread : {:?}", map_senders.keys());
+// }
